6e5113a0cf85baa9c1eb61bb0aa3a3665be5e9cb
Neil Kaushikkar <neil.kaushikkar@gmail.com>
2022-09-27 17:25:25 -0500

    Restructure signal handling

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,36 @@
+use signal_hook::iterator::SignalsInfo;
 use signal_hook::{consts::SIGINT, consts::SIGUSR1, iterator::Signals};
 use std::sync::{Arc, atomic::AtomicBool, atomic::Ordering};
 use std::io::{Read, Seek, Write, SeekFrom, Error};
 use dd_rs::{Metrics, Arguments, Input, Output};
 use clap::Parser;
+use std::thread;
 
 type ExitCondition = Box<dyn Fn(usize, usize) -> bool>;
+
+fn signal_handler(mut signals: SignalsInfo, got_sigusr1: Arc<AtomicBool>, got_sigint: Arc<AtomicBool>) {
+    for sig in signals.forever() {
+        match sig {
+            SIGUSR1 => got_sigusr1.store(true, Ordering::Relaxed),
+            SIGINT => got_sigint.store(true, Ordering::Relaxed),
+            _ => ()
+        } 
+    }
+}
 
 fn main() -> Result<(), Error> {
     let mut args = Arguments::parse();
     args.resolve();
 
     // Set up signal handling and atomic states
-    let should_print: Arc<AtomicBool> = Arc::new(AtomicBool::new(false));
-    let should_print_clone: Arc<AtomicBool> = Arc::clone(&should_print);
-
-    let should_exit: Arc<AtomicBool> = Arc::new(AtomicBool::new(false));
-    let should_exit_clone: Arc<AtomicBool> = Arc::clone(&should_exit);
+    let got_sigusr1: Arc<AtomicBool> = Arc::new(AtomicBool::new(false));
+    let got_sigint: Arc<AtomicBool> = Arc::new(AtomicBool::new(false));
+    let got_sigusr1c: Arc<AtomicBool> = Arc::clone(&got_sigusr1);
+    let got_sigintc: Arc<AtomicBool> = Arc::clone(&got_sigint);
 
     // Spawn signal handler thread
-    let mut signals = Signals::new(&[SIGINT, SIGUSR1])?;
-    std::thread::spawn(move || {
-        for sig in signals.forever() {
-            match sig {
-                SIGUSR1 => should_print_clone.store(true, Ordering::Relaxed),
-                SIGINT => should_exit_clone.store(true, Ordering::Relaxed),
-                _ => ()
-            } 
-        }
-    });
+    let signals = Signals::new(&[SIGINT, SIGUSR1])?;
+    thread::spawn(move || signal_handler(signals, got_sigusr1c, got_sigintc));
 
     // Set up input, output and read buffer
     let mut input = Input::open(args.get_if_path())?;
@@ -47,7 +50,7 @@ fn main() -> Result<(), Error> {
     }
     
     // Define out exit condition depending if there is a max block limit
-    let exit_condition: ExitCondition = match args.get_count() {
+    let should_exit: ExitCondition = match args.get_count() {
         Some(max_blocks) => {
             Box::new(move |blocks_read: usize, bytes_read: usize| {
                 blocks_read == max_blocks || bytes_read == 0
@@ -60,12 +63,12 @@ fn main() -> Result<(), Error> {
     let mut block_counter = 0;
     let result = loop {
         let bytes_read = input.read(&mut buffer)?;
-        if exit_condition(block_counter, bytes_read) || should_exit.load(Ordering::Relaxed) {
+        if should_exit(block_counter, bytes_read) || got_sigint.load(Ordering::Relaxed) {
             break metrics.get_snapshot();
         }
         
-        if should_print.load(Ordering::Relaxed) {
-            should_print.store(false, Ordering::Relaxed);
+        if got_sigusr1.load(Ordering::Relaxed) {
+            got_sigusr1.store(false, Ordering::Relaxed);
             println!("{}", metrics.get_snapshot());
         }
 
